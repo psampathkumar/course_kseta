feaaebb7f417cc50528d33ad403640335534b013
Pranav S <pranav.sampathkumar@gmail.com>
2024-10-17 13:39:27 +0200

    False Check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 data = np.loadtxt("data.csv")
+assert False
 plt.plot(np.arange(20), np.poly1d(np.polyfit(data[:,0], data[:,1], 1))(np.arange(20)), color='r')
 plt.scatter(data[:,0], data[:,1])
 plt.xlabel('x')
