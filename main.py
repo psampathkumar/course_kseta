18647ab48a9b23cc65874371d9f63e21920d2a2a
David Gancarcik <davidgancarcik@dgancarc-1.dyndns.cern.ch>
2024-10-18 14:41:53 +0200

    Adding a simple test

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 data = np.loadtxt("data.csv")
-plt.plot(np.arange(20), np.poly1d(np.polyfit(data[:,0], data[:,1], 1))(np.arange(20)), color='r')
-plt.scatter(data[:,0], data[:,1])
+# Fit a linear regression line to the data
+m, b = np.polyfit(data[:,0], data[:,1], 1)
+# Generate x values for plotting the line
+x_plot = np.linspace(data[:,0].min(), data[:,0].max(), 100)
+# Calculate y values for plotting the line
+y_plot = m*x_plot + b
+
+# Plotting the data points
+plt.scatter(data[:,0], data[:,1], label='Data Points')
+# Plotting the linear regression line
+plt.plot(x_plot, y_plot, color='r', label='Linear Regression Line')
 plt.xlabel('x')
 plt.ylabel('y')
+plt.legend()
 plt.savefig("plot.png")
+
+# Simple test for the code
+assert len(data) > 0, "No data loaded"
